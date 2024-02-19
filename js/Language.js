5a530bf29e5129c1a979957ac1d27d8840b27ba0
Dilses <153410822+Dilses@users.noreply.github.com>
2024-02-19 18:45:23 +0800

    Add files via upload

--- js/Language.js
+++ js/Language.js
@@ -7,42 +7,49 @@
     return parts.slice(0, 2).join('-');
   }
   var localization = document.querySelectorAll('*');
-  var userLanguage = getSubstringBeforeSecondDash(navigator.languages[0] || navigator.language || navigator.userLanguage).toLowerCase();
-  map = {
-    "zh": "zh-cn",
-    "en": "en-us",
-    "af": "af-za",
-    "ar": "ar-ae",
-    "be": "be-by",
-    "bg": "bg-bg",
-    "ca": "ca-es",
-    "cs": "cs-cz",
-    "cy": "cy-gb",
-    "da": "da-dk",
-    "de": "de-de",
-    "dv": "dv-mv",
-    "el": "el-gr",
-    "es": "es-es",
-    "et": "et-ee",
-    "fi": "fi-fi",
-    "fr": "fr-fr",
-    "ga": "ga-ie",
-    "he": "he-il",
-    "hi": "hi-in",
-    "hr": "hr-hr",
-    "hu": "hu-hu",
-    "id": "id-id",
-    "is": "is-is",
-    "it": "it-it",
-    "ja": "ja-jp",
-    "zh-hans": "zh-cn",
-    "zh-hant": "zh-tw",
-    "zh-hk": "zh-tw",
-    "zh-mo": "zh-tw",
-    "zh-sg": "zh-tw"
+  var userLanguage;
+  if (localStorage.getItem('userLanguage')) {
+    userLanguage = localStorage.getItem('userLanguage');
   }
-  if (userLanguage in map) {
-    userLanguage = map[userLanguage];
+  else {
+    userLanguage = getSubstringBeforeSecondDash(navigator.languages[0] || navigator.language || navigator.userLanguage).toLowerCase();
+    map = {
+      "zh": "zh-cn",
+      "en": "en-us",
+      "af": "af-za",
+      "ar": "ar-ae",
+      "be": "be-by",
+      "bg": "bg-bg",
+      "ca": "ca-es",
+      "cs": "cs-cz",
+      "cy": "cy-gb",
+      "da": "da-dk",
+      "de": "de-de",
+      "dv": "dv-mv",
+      "el": "el-gr",
+      "es": "es-es",
+      "et": "et-ee",
+      "fi": "fi-fi",
+      "fr": "fr-fr",
+      "ga": "ga-ie",
+      "he": "he-il",
+      "hi": "hi-in",
+      "hr": "hr-hr",
+      "hu": "hu-hu",
+      "id": "id-id",
+      "is": "is-is",
+      "it": "it-it",
+      "ja": "ja-jp",
+      "zh-hans": "zh-cn",
+      "zh-hant": "zh-tw",
+      "zh-hk": "zh-tw",
+      "zh-mo": "zh-tw",
+      "zh-sg": "zh-tw"
+    }
+    if (userLanguage in map) {
+      userLanguage = map[userLanguage];
+    }
+    localStorage.setItem('userLanguage', userLanguage);
   }
   jsonData = '';
   jmp = true;
