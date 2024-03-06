2fb686b94e0e840d7493b2796123e2fc3e8b20f2
Dilses <153410822+Dilses@users.noreply.github.com>
2024-03-06 23:02:49 +0800

    Add files via upload

--- js/AnimationTitle.js
+++ js/AnimationTitle.js
@@ -1,3 +1,7 @@
+let now = 0;
+let limit;
+let delays;
+
 new Promise((resolve, reject) => {
     const intervalId = setInterval(() => {
         if (window.locTex != undefined) {
@@ -7,19 +11,33 @@ new Promise((resolve, reject) => {
     }, 1000);
 }).then(() => {
     if ('STRING.GLOBAL.TITLE.ANIMATION.LIMIT' in window.locTex) {
-        var limit = window.locTex['STRING.GLOBAL.TITLE.ANIMATION.LIMIT'];
+        limit = window.locTex['STRING.GLOBAL.TITLE.ANIMATION.LIMIT'];
+        delays = new Array(limit);
         var delay = 200;
         if ('STRING.GLOBAL.TITLE.ANIMATION.DELAY' in window.locTex) {
             delay = window.locTex['STRING.GLOBAL.TITLE.ANIMATION.DELAY'];
         }
-        var now = 0;
-        setInterval(() => {
-            if (now >= limit) {
-                now = 0
+        for (let i = 0; i < limit; i++) {
+            if (`STRING.GLOBAL.TITLE.ANIMATION.DELAY.${i}` in window.locTex) {
+                delays[i] = window.locTex[`STRING.GLOBAL.TITLE.ANIMATION.DELAY.${i}`];
             }
-            document.title = `STRING.GLOBAL.TITLE.ANIMATION.${now}`;
-            refreshTitle();
-            now++;
-        }, delay);
+            else {
+                delays[i] = delay;
+            }
+        }
+        animationTitle();
     }
 })
+function animationTitle() {
+    function exec() {
+        document.title = `STRING.GLOBAL.TITLE.ANIMATION.${now}`;
+        refreshTitle();
+        now++;
+        if (now >= limit) {
+            now = 0
+        }
+        animationTitle();
+    }
+
+    setTimeout(exec, delays[now]);
+}
