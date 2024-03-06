6f2004e3531fc7c2b34a5c8d2bd701fe3298a0d8
Dilses <153410822+Dilses@users.noreply.github.com>
2024-03-06 22:42:25 +0800

    Add files via upload

--- js/AnimationTitle.js
+++ js/AnimationTitle.js
@@ -1,27 +1,25 @@
 new Promise((resolve, reject) => {
     const intervalId = setInterval(() => {
-      if (window.locTex != undefined) {
-        clearInterval(intervalId);
-        resolve();
-      }
-    }, 1000);
-  })
-if ('STRING.GLOBAL.TITLE.ANIMATION.LIMIT' in window.locTex)
-{
-    var limit = window.locTex['STRING.GLOBAL.TITLE.ANIMATION.LIMIT'];
-    var delay = 200;
-    if ('STRING.GLOBAL.TITLE.ANIMATION.DELAY' in window.locTex)
-    {
-        delay = window.locTex['STRING.GLOBAL.TITLE.ANIMATION.DELAY'];
-    }
-    var now = 0;
-    setInterval(() => {
-        if (now >= limit)
-        {
-            now = 0
+        if (window.locTex != undefined) {
+            clearInterval(intervalId);
+            resolve();
         }
-        document.title = `STRING.GLOBAL.TITLE.ANIMATION.${now}`;
-        refreshTitle();
-        now++;
-    }, delay);
-}
+    }, 1000);
+}).then(() => {
+    if ('STRING.GLOBAL.TITLE.ANIMATION.LIMIT' in window.locTex) {
+        var limit = window.locTex['STRING.GLOBAL.TITLE.ANIMATION.LIMIT'];
+        var delay = 200;
+        if ('STRING.GLOBAL.TITLE.ANIMATION.DELAY' in window.locTex) {
+            delay = window.locTex['STRING.GLOBAL.TITLE.ANIMATION.DELAY'];
+        }
+        var now = 0;
+        setInterval(() => {
+            if (now >= limit) {
+                now = 0
+            }
+            document.title = `STRING.GLOBAL.TITLE.ANIMATION.${now}`;
+            refreshTitle();
+            now++;
+        }, delay);
+    }
+})
